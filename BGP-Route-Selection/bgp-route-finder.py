9a886bd5c63338874490fdbeead3096d172ab70f
snadeem03 <shaiknadeem2003@gmail.com>
2024-02-24 22:48:08 +0530

    Enhanced findBestRoute function with command-line argument capability

--- BGP-Route-Selection/bgp-route-finder.py
+++ BGP-Route-Selection/bgp-route-finder.py
@@ -1,37 +1,41 @@
-def findBestRoute(route1,route2):
-    
-    # check for the local preferences
-    if route1['local_preference']>route2['local_preference']:
+import argparse
+
+def find_best_route(route1, route2):
+    # Check for the local preferences
+    if route1['local_preference'] > route2['local_preference']:
         return route1
-    elif route1['local_preference']<route2['local_preference']:
+    elif route1['local_preference'] < route2['local_preference']:
         return route2
-    else :
-        # check for the AS_PATH
-            
+    else:
+        # Check for the AS_PATH
         if len(route1['as_path']) > len(route2['as_path']):
             return route2
         elif len(route1['as_path']) < len(route2['as_path']):
             return route1
-        else :
+        else:
             return route1
-            
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Find the best route between two routes.')
+    parser.add_argument('route1_as_path', type=int, nargs='+', help='AS_PATH of route1')
+    parser.add_argument('route1_local_preference', type=int, help='Local preference of route1')
+    parser.add_argument('route2_as_path', type=int, nargs='+', help='AS_PATH of route2')
+    parser.add_argument('route2_local_preference', type=int, help='Local preference of route2')
+    args = parser.parse_args()
 
-
-if __name__=='__main__':
-    
-    route1_info={
-        'prefix':'192.168.1.0/24',
-        'as_path': [100, 200],
-        'next_hop': '10.0.0.1',
-        'local_preference': 200
-    }    
-    
-    route2_info={
+    route1_info = {
         'prefix': '192.168.1.0/24',
-        'as_path': [200],
-        'next_hop': '10.0.0.2',
-        'local_preference': 200
+        'as_path': args.route1_as_path,
+        'next_hop': '10.0.0.1',
+        'local_preference': args.route1_local_preference
     }
-    
-    print(findBestRoute(route1_info, route2_info))
+
+    route2_info = {
+        'prefix': '192.168.1.0/24',
+        'as_path': args.route2_as_path,
+        'next_hop': '10.0.0.2',
+        'local_preference': args.route2_local_preference
+    }
+
+    best_route = find_best_route(route1_info, route2_info)
+    print(best_route)
